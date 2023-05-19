ecea6c653fac8f01780a80a74a0935d879b1bd6f
Thariq Ahamed <114891365+itzthariq@users.noreply.github.com>
2023-05-19 22:22:32 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("hello world again")
+print("i like typing tis")
+print("this is not fun")
+print('yay! printing.')
+print("i'd much rather you 'not'.")
+print('i "said" do not touch this')
