7751d968c2254624bc9f1b25c65e73a94abe4e50
Thariq Ahamed <114891365+itzthariq@users.noreply.github.com>
2023-05-19 22:31:50 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+#Comment and pound charecters
+#a comment this is so you can read your program later 
+print("i could have code like tis.") #and code after tis is ignored.
+#you can also use a comment to "disable" or comment out code:
+print("tis won't run")
