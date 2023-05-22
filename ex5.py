213a459d09790f9c87e0a10709e63b77053d8b6d
Thariq Ahamed <114891365+itzthariq@users.noreply.github.com>
2023-05-22 23:45:04 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,21 @@
+#MORE VARIABLES AND PRINTING
+my_name = 'Thariq'
+my_age ='22'
+my_height ='5.1'
+my_weight ='50'
+my_eyes ='brown'
+my_teeth ='white'
+my_hair ='black'
+
+print(f"my name is {my_name}")
+print(f"its my age {my_age}")
+print(f"its my height{my_height}")
+print(f"its my weight{my_weight}")
+print(f"clr of my eyes is {my_eyes}")
+print(f"clr of my teeth is{my_teeth}")
+print(f"clr of my my hair is {my_hair}")
+print(f"am using beer shampoo for my shining {my_hair} hair and using closeup paste for my {my_teeth} teeth")
+
+#this is tricky line so try it carefully
+total=my_age+my_height+my_weight
+print(f"if i add {my_age},{my_height},{my_weight} i get {total}")
